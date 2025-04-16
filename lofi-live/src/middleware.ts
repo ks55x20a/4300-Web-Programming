fd263030af1b1ec5ccc8b246920bc98531e7f243
Jackson Antulov <antulovjackson@gmail.com>
2025-04-16 14:35:49 -0400

    Added basic auth implementation and db schema

---
+++ lofi-live/src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server"
+import { authConfig } from "./auth.config"
+import NextAuth from "next-auth"
+
+const { auth } = NextAuth(authConfig);
+
+const middleware = async (request: NextRequest ) => {
+    const { pathname } = request.nextUrl;
+    const session = await auth();
+    const isAuthenticated = !!session?.user;
+    console.log(isAuthenticated, pathname);
+
+    const publicPaths = ["/", "/account", "/concerts"];
+
+    if (!isAuthenticated && !publicPaths.includes(pathname)) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        "/dashboard/:path*"
+    ]
+}
+
+export default middleware;
